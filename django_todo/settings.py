e2b6011c700fa1ea62a61578387aedcda076d7ae
chrisoc19 <chrisoconnor904@yahoo.com.au>
2020-01-10 12:42:55 +0000

    Update settings to use env var

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f81!luxs+_qb8^xq((@*cf5#nnouoct@2+jorhjek@*&53r!9r'
+SECRET_KEY = os.environ.get('SECRET_KEY','f81!luxs+_qb8^xq((@*cf5#nnouoct@2+jorhjek@*&53r!9r')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['plain-django-todo.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('GITPOD_HOSTNAME'),
+                 os.environ.get('HOSTNAME')]
 
 
 # Application definition
--- todo/test_forms.py
+++ todo/test_forms.py
@@ -6,7 +6,7 @@ from .forms import ItemForm
 class TestToDoItemForm(TestCase):
 
     def test_can_create_an_item_with_just_a_name(self):
-        form = ItemForm({'name': 'Create Tests'})
+        form = ItemForm({'name': 'Create Test'})
         self.assertTrue(form.is_valid())
 
     def test_correct_message_for_missing_name(self):
